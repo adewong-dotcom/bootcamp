e056719428206fe053d8e08b3f286df0b3088d6d
Andrea de Wong <am.dewong@gmail.com>
2021-12-04 02:55:20 -0600

    Bootstrap implemented

--- project001/index.js
+++ project001/index.js
@@ -76,7 +76,16 @@ function onSubmit(event)
         <td>${cantidad}</td>
         <td>${precio}</td>
         <td>${total}</td>
-        <td><a href="#" onclick = "onEdit(event)">Editar</a> | <a href="#" onclick = "onDelete(event)">Eliminar</a></td>
+        <td>
+            <div class="btn-group">
+                <a title="Editar" href="#" onclick = "onEdit(event)" class="btn btn-sm btn-outline-secondary">
+                    <i class="bi bi-pencil-square"></i>
+                </a> 
+                <a title="Eliminar" href="#" onclick = "onDelete(event)" class="btn btn-sm btn-outline-danger>
+                    <i class="bi bi-trash"></i>
+                </a>
+            </div>
+        </td>
     `;
 
     cantidadTotalElement.innerText = cantidadTotal;
@@ -99,8 +108,8 @@ function onEdit (event){
     /**
      * @type {HTMLAnchorElement}
      */
-    const anchor = event.target;
-    const tr = anchor.parentElement.parentElement;
+    const anchor = event.currentTarget;
+    const tr = anchor.parentElement.parentElement.parentElement;
     const celdas = tr.getElementsByTagName("td");
     const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
 
@@ -123,8 +132,8 @@ function onDelete (event)
       /**
      * @type {HTMLAnchorElement}
      */
-       const anchor = event.target;
-       const tr = anchor.parentElement.parentElement;
+       const anchor = event.currentTarget;
+       const tr = anchor.parentElement.parentElement.parentElement;
        tbody.removeChild(tr);
        
 }
